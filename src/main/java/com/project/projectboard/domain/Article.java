a26664ca456b930d822edab3b62feb377568f9fc
bellring <starringbell@gmail.com>
2023-07-03 18:04:02 +0900

    #10 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로 사용될 것으로 보이므로 별도로 분리
    @EntityListeners(AuditingEntityListener.class)
    @MappedSuperclass

--- src/main/java/com/project/projectboard/domain/Article.java
+++ src/main/java/com/project/projectboard/domain/Article.java
@@ -1,20 +1,14 @@
 package com.project.projectboard.domain;
 
 import lombok.*;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "title"),
         @Index(columnList = "hashtag"),
@@ -23,8 +17,7 @@ import java.util.Set;
 })
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(AuditingEntityListener.class)
-public class Article {
+public class Article extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -45,21 +38,7 @@ public class Article {
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
-
-    //jpa auditing
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy;
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt;
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy;
-
+    
     private Article(String title, String content, String hashtag) {
         this.title = title;
         this.content = content;
