e975eab810c90e8440d0131c43bed38bd2816d9c
bellring <starringbell@gmail.com>
2023-07-04 22:58:43 +0900

    #14 - 게시글 페이지 테스트
    기본 골격 작성 후 뷰 테스트 까지 적용.

--- src/test/java/com/project/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/project/projectboard/controller/ArticleControllerTest.java
@@ -37,7 +37,7 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
+
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     void given_whenRequestingArticleView_thenReturnArticleView() throws Exception {
